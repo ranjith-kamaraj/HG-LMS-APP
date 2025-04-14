5aff3f4accf17014c08df579e280068a9f83a05c
Ranjith Kamaraj <42859314+ranjith-kamaraj@users.noreply.github.com>
2025-04-14 18:01:26 -0400

    Update server.ts

--- app-server/src/server.ts
+++ app-server/src/server.ts
@@ -21,13 +21,13 @@ global.lms = {
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({
-    message: "Hello Worllddddddddddd!!!!!",
+    message: "Hello Worllddddddddddd---------------!!!!!",
   });
 });
 
 app.get("/hi", (req: Request, res: Response) => {
   res.status(200).send({
-    message: "Hello World Hiiiiiiiiiiii!!!!!",
+    message: "Hello World Hiiiiiiiiiiii-------------------!!!!!",
   });
 });
 
